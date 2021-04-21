ff1ca7527d141a432f25dceb4ca0cbf6b0e8ccbb
Josh Liu <yunzheli@usc.edu>
2021-04-21 00:14:52 -0700

    Initial Commit

---
+++ hw/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  hw
+//
+//  Created by Josh Liu on 4/21/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
